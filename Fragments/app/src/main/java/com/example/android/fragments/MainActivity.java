808f2bee326763e92ea996a70763457d92e13cd7
NishantKr97 <nishantkr1997@gmail.com>
2018-02-26 01:52:34 +0530

    Fragments

---
+++ Fragments/app/src/main/java/com/example/android/fragments/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.android.fragments;
+
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.MotionEvent;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView t1 = (TextView) findViewById(R.id.tab1);
+//        t1.setText("Hello Tab1");
+//        Typeface tfc1 = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Bold.ttf");
+//        t1.setTypeface(tfc1);
+
+
+        TabLayout tabLayout = (TabLayout)  findViewById(R.id.tablayout);
+        tabLayout.setHorizontalScrollBarEnabled(true);
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+//
+//    @Override
+//    public void onFragmentInteract  ion(Uri uri) {
+//
+//    }
+}
